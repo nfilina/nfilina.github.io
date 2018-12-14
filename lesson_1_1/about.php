4976c59675780f95a2914313cf1feee9d21eac28
nfilina <45875694+nfilina@users.noreply.github.com>
2018-12-14 23:07:13 +0700

    Создала 1_1 задание
    Закачала файл

---
+++ lesson_1_1/about.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<meta charset="UTF-8">
+	<title>Введение в PHP</title>
+	<style>
+	body {
+       font-family: 'Helvetica Neue',Helvetica,Arial,sans-serif;  
+    }
+    </style>
+</head>
+<body>
+<?php 
+
+	echo "<h1>Добро пожаловать на страницу пользователя Наталья</h1>";
+
+?>
+	
+    <dl>
+	<dt>Имя</dt>
+	<dd>Наталья</dd>
+	<dt>Возраст</dt>
+	<dd>200</dd>
+	<dt>Адрес электронной почты</dt>
+	<dd><a href="mailto:fitasha@mail.ru">fitasha@mail.ru</a></dd>
+	<dt>Город</dt>
+	<dd>Новосибирск</dd>
+	<dt>О себе</dt>
+	<dd>Full stack разработчик</dd>
+	</dl>	
+</body>
+</html>
+
+
